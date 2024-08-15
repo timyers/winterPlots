93b058441d73cbe3f8546ccd5c84e4ec233472e2
Tim <48717893+timyers@users.noreply.github.com>
2024-08-15 14:10:18 -0400

    add new script

--- R/bed_liftoverR.R
+++ R/bed_liftoverR.R
@@ -1,3 +1,4 @@
-# Description: convert bed file with hg38 coordinates
-# to hg19 and save to text file.
+# Description: function to convert bed file with hg38 
+# coordinates to hg19 and save to text file.
+
 
