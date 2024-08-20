108813ff273d25e1260a6ba7605ede76e9b6a1de
timyers <48717893+timyers@users.noreply.github.com>
2024-08-20 14:19:39 -0400

    add helper function to check and fix column names

--- R/bed_liftoverR.R
+++ R/bed_liftoverR.R
@@ -5,7 +5,8 @@ library(rtracklayer)
 library(GenomicRanges)
 library(R.utils)
 
-# load function
+#######################
+# load convert function
 convert_hg38_to_hg19 <- function(bed_data) {
   
   # Convert the data frame to a GRanges object
@@ -38,8 +39,31 @@ convert_hg38_to_hg19 <- function(bed_data) {
   
   return(result)
 }
+######## End convert main function ##########
 
-# Example usage
+#########################
+# load helper function to fix column names if necessary
+fix_bed_columns <- function(df) {
+  # Define the correct column names
+  expected_columns <- c("chrom", "chromStart", "chromEnd", "score")
+  
+  # Check if the columns match the expected ones
+  if (!all(colnames(df) == expected_columns)) {
+    message("Column names do not match the expected BED format. Renaming columns...")
+    colnames(df) <- expected_columns
+  } else {
+    message("Column names are correct.")
+  }
+  
+  return(df)
+}
+# Example usage:
+# Assuming you have a data frame `bed_df`
+# bed_df <- check_and_fix_bed_columns(bed_df)
+
+############## End helper fix column function #############
+
+# Example usage of main function
 bed_data <- data.frame(
   chrom = c("chr15", "chr15", "chr15"),
   chromStart = c(20100537, 20100540, 20100654),
@@ -49,9 +73,17 @@ bed_data <- data.frame(
 
 # read in file
 file_path <- "data/chip-atlas/subset_chr15_hg38_parsed_Histone.Kidney.100.H3K27ac.AllCell.bed.txt"
+# Or
+file_path <- "data/chip-atlas/filtered_chip-atlas_H3K27ac_HEK293T_hg38.bed"
+# Orr
+file_path <- "data/chip-atlas/filtered_chip-atlas_H3K27ac_ACHN_hg38.bed"
+# read file
 df_bed <- data.table::fread(file_path, sep = "\t", header = TRUE)
 
-# call function to convert coordinates
+# call helper function to check for correct column names
+df_bed <- fix_bed_columns(df_bed)
+
+# call main function to convert coordinates
 result_hg19 <- convert_hg38_to_hg19(df_bed)
 
 # write file converted to hg19
@@ -64,5 +96,20 @@ write.table(result_hg19, file = file_path_hg19,
             col.names = TRUE, quote = FALSE
            )
 
+### Or
+chip_file_path <- file_path <- "data/chip-atlas/"
+full_file_name <- paste0(chip_file_path,
+                         "filtered_chip-atlas_",
+                         "H3K27ac_",
+                         # "HEK293T_",
+                         "ACHN_",
+                         "hg19",
+                         ".bed"
+                         )
+
+write.table(result_hg19, file = full_file_name,
+            sep = "\t", row.names = FALSE,
+            col.names = TRUE, quote = FALSE
+            )
 
 print(result_hg19)
